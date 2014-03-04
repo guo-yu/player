2437909eb788d28377039d6376a12ef1e5d15119
Guo Yu <i@guoyu.me>
2014-03-04 11:51:51 +0800

    0.1.4: demo.js fixed

--- demo/demo.js
+++ demo/demo.js
@@ -1,13 +1,12 @@
 var Player = require('../index');
 
-// 播放
 var player = new Player([
     __dirname + '/demo.mp3',
     __dirname + '/demo2.mp3'
 ]);
 
 player.play(function(err, p){
-    console.log('播放完了！');
+    console.log('all songs play end');
 });
 
 player.on('downloading',function(item){
@@ -17,18 +16,13 @@ player.on('downloading',function(item){
 player.on('playing',function(item){
     console.log('im playing... ');
     console.log(item);
-    setTimeout(function(){
-        player.stop()
-    },2000)
 });
 
 player.on('playend',function(item){
     console.log('play done, switching to next one ...');
-    player.add('http://zhangmenshiting.baidu.com/data2/music/10470876/7343701219600128.mp3?xcode=351634fd3718fed5abb2f9389b9d4097b9319fcd4157c2b2')
 });
 
 player.on('error', function(err){
-    // 当流媒体出现播放错误时
-    console.log('Opps... 流媒体发生错误!')
+    console.log('Opps...!')
     console.log(err);
 });
