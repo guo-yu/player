48afcec268fcb053962f73d6c9f0338db2a1b07d
Guo Yu <i@guoyu.me>
2014-12-03 12:24:29 +0800

    0.3: Update stream example

--- examples/stream.js
+++ examples/stream.js
@@ -6,17 +6,11 @@ var player = new Player('http://stream.srg-ssr.ch/m/rsp/mp3_128', {
   stream: true
 });
 
-player.play(function(err) {
-  debug('All songs play end');
-});
+player.play();
 
 player.on('playing', function(song) {
   debug('Playing... ');
   debug(song);
-});
-
-player.on('playend', function(song) {
-  debug('Playend');
 });
 
 player.on('error', function(err) {
