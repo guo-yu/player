8d6d0e178a10b8c33e67e6c980b8e68a07533188
Yiting <yli229@uic.edu>
2015-04-11 16:05:12 -0700

    rm unrelated files

--- examples/test_metadata.js
+++ examples/test_metadata.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var mm = require('musicmetadata');
 
-var parser = mm(fs.createReadStream('Breeze.mp3'),{ duration: true }, function (err, metadata) {
+var parser = mm(fs.createReadStream('demo3_with_metadata.mp3'),{ duration: true }, function (err, metadata) {
     if (err) {
         console.log(err);
     }
