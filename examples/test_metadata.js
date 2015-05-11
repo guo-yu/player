b35ef55be4e792bcfae776b4e4cd0d541760eec9
Guo Yu <i@guoyu.me>
2015-05-11 16:09:20 +0800

    Update example meta

--- examples/test_metadata.js
+++ examples/test_metadata.js
@@ -1,11 +1,15 @@
-var fs = require('fs');
-var mm = require('musicmetadata');
+var path = require('path');
+var Player = require('../dist/player');
+var pkg = require('../package.json');
+var debug = require('debug')(pkg.name);
 
-mm(fs.createReadStream('demo3_with_metadata.mp3'), {
-  duration: true
-}, function(err, metadata) {
-  if (err)
-    console.log(err)
-
-  console.log(metadata)
-})
+new Player(path.join(__dirname, 'demo3_with_metadata.mp3'))
+  .on('playing', function(song) {
+    debug('Playing... ');
+    debug(song.meta);
+  })
+  .on('error', function(err) {
+    debug('Opps...!')
+    debug(err);
+  })
+  .play()
