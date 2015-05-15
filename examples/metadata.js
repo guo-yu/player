77c61fac41dfb8f5806f3827e2fb873541b4c593
Guo Yu <i@guoyu.me>
2015-05-15 17:42:26 +0800

    Update example metadata

--- examples/metadata.js
+++ examples/metadata.js
@@ -5,15 +5,15 @@ var debug = require('debug')(pkg.name);
 
 new Player(path.join(__dirname, './mp3/demo3_with_metadata.mp3'))
   .on('playing', function(song) {
-    debug('Playing... ');
-    debug(song.meta);
+    debug('Playing... ')
+    debug(song.meta)
 
     // Show formated metadata to `stdout`
     if (song.meta)
-      this.show(song.meta);
+      this.progress(song.meta)
   })
   .on('error', function(err) {
     debug('Opps...!')
-    debug(err);
+    debug(err)
   })
   .play()
